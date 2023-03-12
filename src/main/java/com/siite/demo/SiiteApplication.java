086634062b9474dc35ad8d6a2b1a400e39496438
E. P <eva.pankevica@gmail.com>
2023-03-12 23:48:32 +0200

    inicialise project

---
+++ src/main/java/com/siite/demo/SiiteApplication.java
@@ -0,0 +1,13 @@
+package com.siite.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siite/demo/SiiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siite.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
