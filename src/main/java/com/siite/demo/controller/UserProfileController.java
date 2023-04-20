fba66ecd677fcda926b1b52a31d8bd2264d4ec3e
E. P <eva.pankevica@gmail.com>
2023-04-21 01:36:38 +0300

    getUserWebsites in UserProfileController

--- src/main/java/com/siite/demo/controller/UserProfileController.java
+++ src/main/java/com/siite/demo/controller/UserProfileController.java
@@ -2,7 +2,11 @@ package com.siite.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.siite.demo.models.MyUser;
 import com.siite.demo.services.IMyUserCRUDservice;
 import com.siite.demo.services.IMyWebsiteCRUDservice;
 
@@ -15,9 +19,15 @@ public class UserProfileController {
 	@Autowired
 	private IMyWebsiteCRUDservice websiteService;
 	
-	public String getUserWebsites() {
-		
-		return "user-profile/user-profile";
+	@GetMapping("/user/{id}")
+	public String getUserWebsites(Model model, MyUser user, @PathVariable(name = "id") int id) {
+		try {
+			model.addAttribute("user", userService.readUserById(id));
+			model.addAttribute("website", websiteService.getUserWebsitesbyUserId(id));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "userprofile/user-profile";
 	}
 
 }
